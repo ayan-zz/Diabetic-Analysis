2aea3f379555ad499157f601f3ad8ea0473c1d9e
ayan-zz <64850346+ayan-zz@users.noreply.github.com>
2021-03-30 23:11:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,12 +24,13 @@ def predict():
         DiabetesPedgreeFunction=float(request.form['DiabetesPedgreeFunction'])
         Age=int(request.form['Age'])
         predictions=model.predict([[Pregnancies, Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedgreeFunction,Age]])
-        prediction_text=predictions
-        if output==1:
+        output=predictions
+        if output>=1:
             return render_template('index.html', prediction_text= predictions)
         else:
             return render_template('index.html', prediction_text=predictions)
-        else:
+        
+      else:
             return render_template('index.html')
         
        
