fddc64d42f203afc0537fce5f749d1898fb6615d
ayan-zz <64850346+ayan-zz@users.noreply.github.com>
2021-03-30 23:25:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+#app.py
+from flask import Flask,request,render_template
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+app = Flask(__name__)
+model = pickle.load(open('diabetes_regression_model.pkl', 'rb'))
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    if request.method=='POST':
+        Pregnancies=int(request.form['Pregnancies'])
+        Glucose=float(request.form['Glucose'])
+        BloodPressure=float(request.form['BloodPressure'])
+        SkinThickness=float(request.form['SkinThickness'])
+        Insulin=float(request.form['Insulin'])
+        BMI=float(request.form['BMI'])
+        DiabetesPedgreeFunction=float(request.form['DiabetesPedgreeFunction'])
+        Age=int(request.form['Age'])
+        predictions=model.predict([[Pregnancies, Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedgreeFunction,Age]])
+        if predictions>=1:
+            return render_template('index.html', prediction_text= predictions)
+        else:
+            return render_template('index.html', prediction_text="Non-diabetic.\n Stay fit and Healthy")
+    else:
+        return render_template('index.html')
+    
+if __name__=="__main__":
+    app.run(debug=True,use_reloader=False)
+    
+
+        
+        
+            
